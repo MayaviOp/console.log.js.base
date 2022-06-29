b9c97e1cd412696d0ed539c27a832b7b0d80a678
MayaviOp <108415042+MayaviOp@users.noreply.github.com>
2022-06-29 20:25:14 +0530

    Create console.js

---
+++ console.js
@@ -0,0 +1,17 @@
+// Bais
+console.log("token");
+console.log("hi", "hll", `for`, 'mayavi');
+// Dis is faction
+const not = "fo not w time";
+const j = "anjoom";
+const hacker = "Momo";
+const what = 8+1;
+
+// Dis Is "Log"
+console.log(not, j, hacker);
+console.log("hacker is", hacker);
+console.log(hacker, "are", j);
+console.log(what, j);
+
+console.log();
+
